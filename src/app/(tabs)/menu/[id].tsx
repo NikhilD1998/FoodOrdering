018ff880978543e7ad8af3f1cc0540d97b6bb13e
NikhilD1998 <nikhild.dev.1998@gmail.com>
2024-07-20 19:41:19 +0530

    Showing Menu Item Detail Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,41 @@
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() == id);
+
+  if (!product) {
+    <Text>Product not found</Text>;
+  }
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details" }} />
-      <Text>Products Detail Screen for id: {id}</Text>
+    <View style={styles.continer}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product?.image || defaultPizzaImage }}
+      />
+      <Text style={styles.price}>${product?.price}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  continer: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
 export default ProductDetailsScreen;
